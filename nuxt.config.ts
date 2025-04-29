5c4bfa7fe7d5d317bd9a04d877b2e93a84f26e47
Clay-369 <claytonsum10@gmail.com>
2025-04-29 16:02:28 +0200

    Add tailwindcss to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/fonts', '@nuxt/icon']
-})
+  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/icon"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/main.css"],
+});
