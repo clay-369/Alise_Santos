8957fb8686b7d9a358b0f566445a56b1517cfe8f
Clay-369 <claytonsum10@gmail.com>
2025-05-01 12:30:04 +0200

    Remove information section on Approach.vue

--- components/sections/Approach.vue
+++ components/sections/Approach.vue
@@ -3,12 +3,6 @@
     <div class="w-full lg:w-1/2">
       <h2>Aanpak</h2>
       <h3>Persoonlijke begeleiding op maat</h3>
-      <p>
-        Mijn naam is Alicia Soares, ik bied hulp vanuit mijn hart. Ik kijk samen
-        met jou wat je nodig hebt om je doel te bereiken. Met meer dat 20 jaar
-        ervaring in de jeugd/hulpverlening weet ik hoe ik mensen in hun kracht
-        kan zetten zodat ze zelfstandig verder kunnen.
-      </p>
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
       <div
