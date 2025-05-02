8556cfaccb4d203011fce3c9452c819ed8a852f7
Clay-369 <claytonsum10@gmail.com>
2025-05-02 14:59:23 +0200

    Change name to Alicia Santos Soares

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -2,12 +2,12 @@
   <section class="flex flex-col lg:flex-row gap-10 items-center" id="about">
     <div class="w-full lg:w-1/2">
       <h2>Over mij</h2>
-      <h3>Mijn naam is Alicia Soares</h3>
+      <h3>Mijn naam is Alicia Santos Soares</h3>
       <p>
-        Mijn naam is Alicia Soares, ik bied hulp vanuit mijn hart. Ik kijk samen
-        met jou wat je nodig hebt om je doel te bereiken. Met meer dat 20 jaar
-        ervaring in de jeugd/hulpverlening weet ik hoe ik mensen in hun kracht
-        kan zetten zodat ze zelfstandig verder kunnen.
+        Mijn naam is Alicia Santos Soares, ik bied hulp vanuit mijn hart. Ik
+        kijk samen met jou wat je nodig hebt om je doel te bereiken. Met meer
+        dat 20 jaar ervaring in de jeugd/hulpverlening weet ik hoe ik mensen in
+        hun kracht kan zetten zodat ze zelfstandig verder kunnen.
       </p>
     </div>
     <div>
